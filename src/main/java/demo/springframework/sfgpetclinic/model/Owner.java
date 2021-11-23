4472a2a1ff72214ed3e2841244957952167626b5
kafeincdurmaz <caglar.durmaz@vodafone.com>
2021-11-23 19:14:04 +0300

    adding POJOs. Closes #2.

---
+++ src/main/java/demo/springframework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package demo.springframework.sfgpetclinic.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/demo/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package demo.springframework.sfgpetclinic.model;
+
+public class Vet extends Person{
+}
